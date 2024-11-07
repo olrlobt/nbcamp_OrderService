d790da7b86388953bc9989bf24693b4b7bad08a5
유현 <yh7119@gmail.com>
2024-11-07 16:15:07 +0900

    feat: 공통 응답 구조 생성

---
+++ src/main/java/com/nbcamp/orderservice/global/response/CommonResponse.java
@@ -0,0 +1,36 @@
+package com.nbcamp.orderservice.global.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.nbcamp.orderservice.global.exception.code.ErrorCode;
+import com.nbcamp.orderservice.global.exception.code.SuccessCode;
+
+public record CommonResponse<T>(boolean success, String message, T result) {
+
+	public static <T> ResponseEntity<CommonResponse<T>> success(SuccessCode successCode) {
+		return ResponseEntity.status(successCode.getHttpStatus())
+			.body(new CommonResponse<>(true, successCode.getMessage(), null));
+	}
+
+	public static <T> ResponseEntity<CommonResponse<T>> success(SuccessCode successCode, T data) {
+		return ResponseEntity.status(successCode.getHttpStatus())
+			.body(new CommonResponse<>(true, successCode.getMessage(), data));
+	}
+
+	public static <T> ResponseEntity<CommonResponse<T>> fail(ErrorCode errorCode) {
+		return ResponseEntity.status(errorCode.getHttpStatus())
+			.body(new CommonResponse<>(false, errorCode.getMessage(), null));
+	}
+
+	public static <T> ResponseEntity<CommonResponse<T>> fail(ErrorCode errorCode, T data) {
+		return ResponseEntity.status(errorCode.getHttpStatus())
+			.body(new CommonResponse<>(false, errorCode.getMessage(), data));
+	}
+
+	public static <T> ResponseEntity<CommonResponse<T>> fail(RuntimeException exception) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(new CommonResponse<>(false, exception.getMessage(), null));
+	}
+
+}
