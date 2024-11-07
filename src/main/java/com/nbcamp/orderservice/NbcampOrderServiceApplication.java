8055dd9c8e5bd91b64cb6c1675dec3c1c6fdbde7
olrlobt <317tmdgjs@naver.com>
2024-11-07 14:22:42 +0900

    Initial commit

---
+++ src/main/java/com/nbcamp/orderservice/NbcampOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nbcamp.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbcamp/orderservice/NbcampOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbcamp.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
