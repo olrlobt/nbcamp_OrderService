58e25c0b3168d78fae0f31d01083edf63baf4043
유현 <yh7119@gmail.com>
2024-11-07 15:15:46 +0900

    feat: 매장 엔티티 추가

--- src/main/java/com/nbcamp/orderservice/domain/product/entity/Product.java
+++ src/main/java/com/nbcamp/orderservice/domain/product/entity/Product.java
@@ -4,12 +4,16 @@ import java.util.UUID;
 
 import com.nbcamp.orderservice.domain.common.BaseTimeEntity;
 import com.nbcamp.orderservice.domain.common.DisplayStatus;
+import com.nbcamp.orderservice.domain.store.entity.Store;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -30,6 +34,10 @@ public class Product extends BaseTimeEntity {
 	@Id
 	@Column(name = "id", columnDefinition = "uuid comment '상품 고유 번호'")
 	private UUID id = UUID.randomUUID();
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false, columnDefinition = "uuid comment '매장 고유 번호'")
+	private Store store;
 
 	@Column(name = "name", nullable = false, columnDefinition = "varchar comment '상품명'")
 	private String name;
