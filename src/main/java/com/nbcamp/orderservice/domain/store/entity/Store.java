a9c4b628a5e34a57356131f7a3ed4ffd4145e209
jshstar <jshstar97@gmail.com>
2024-11-08 20:24:59 +0900

    fix: 매장 엔티티 UUID 생성방식 변경

--- src/main/java/com/nbcamp/orderservice/domain/store/entity/Store.java
+++ src/main/java/com/nbcamp/orderservice/domain/store/entity/Store.java
@@ -11,6 +11,8 @@ import com.nbcamp.orderservice.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -33,6 +35,7 @@ import lombok.NoArgsConstructor;
 public class Store extends BaseTimeEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(name = "id")
 	private UUID id = UUID.randomUUID();
 
