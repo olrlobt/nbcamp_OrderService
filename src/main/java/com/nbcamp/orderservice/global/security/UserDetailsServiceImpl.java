34a7fc922071b944d54c88abc987479213a3b9c4
olrlobt <317tmdgjs@naver.com>
2024-11-08 22:03:29 +0900

    feat: Security UserDetails 구현체

---
+++ src/main/java/com/nbcamp/orderservice/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.nbcamp.orderservice.global.security;
+
+import java.util.UUID;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nbcamp.orderservice.domain.user.entity.User;
+import com.nbcamp.orderservice.domain.user.repository.UsersRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UsersRepository usersRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String uuid) throws UsernameNotFoundException {
+		try {
+			UUID uuidConverted = UUID.fromString(uuid);
+			User users = usersRepository.findById(uuidConverted)
+				.orElseThrow(() -> new IllegalArgumentException("User not found with UUID: " + uuid));
+			return new UserDetailsImpl(users);
+		} catch (IllegalArgumentException e) {
+			throw new UsernameNotFoundException("Invalid UUID format: " + uuid, e);
+		}
+	}
+}
