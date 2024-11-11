1410b3a30f853a905cbcc6c850216fd946f74d20
유현 <yh7119@gmail.com>
2024-11-11 16:50:11 +0900

    feat: Id 값 세팅 변경

--- src/main/java/com/nbcamp/orderservice/domain/ai/entity/AIRequestLog.java
+++ src/main/java/com/nbcamp/orderservice/domain/ai/entity/AIRequestLog.java
@@ -11,6 +11,8 @@ import com.nbcamp.orderservice.domain.product.entity.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -32,8 +34,9 @@ import lombok.NoArgsConstructor;
 public class AIRequestLog {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(name = "id")
-	private UUID id = UUID.randomUUID();
+	private UUID id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id", nullable = false)
