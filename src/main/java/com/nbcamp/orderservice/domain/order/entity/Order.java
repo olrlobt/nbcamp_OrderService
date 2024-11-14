761385e9238bce93c819c882ce21a51a7986d556
jshstar <jshstar97@gmail.com>
2024-11-14 20:57:06 +0900

    fix: merge conflict 누락 부분 수정

--- src/main/java/com/nbcamp/orderservice/domain/order/entity/Order.java
+++ src/main/java/com/nbcamp/orderservice/domain/order/entity/Order.java
@@ -18,6 +18,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -41,7 +43,8 @@ public class Order extends BaseTimeEntity {
 
 	@Id
 	@Column(name = "id")
-	private UUID id = UUID.randomUUID();
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "store_id", nullable = false)
