018404c74ada03a7b13bd072e60611ebfb1baa35
jshstar <jshstar97@gmail.com>
2024-11-17 01:07:40 +0900

    fix: 주소 정규식 문제 해결

--- src/main/java/com/nbcamp/orderservice/domain/store/dto/StoreRequest.java
+++ src/main/java/com/nbcamp/orderservice/domain/store/dto/StoreRequest.java
@@ -18,7 +18,7 @@ public record StoreRequest(
 	@Size(max = 200, message = "주소는 200자 이내로 작성해 주세요.")
 	@NotNull(message = "주소는 필수 입력값입니다.")
 	@Pattern(
-		regexp = "([가-힣]+(특별시|광역시|도))\\s([가-힣]+구)",
+		regexp = "([가-힣]+(특별시|광역시|도))\\s([가-힣]+구)(.*)?",
 		message = "주소는 올바른 형식으로 입력해 주세요. 예: 서울특별시 강남구"
 	)
 	String address,
